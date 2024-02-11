75edb211c7e80068323773efe1a8af1c63f6e1cb
Andrew Huynh <hpthien1307@gmail.com>
2024-02-11 12:44:28 +0700

    first commit

---
+++ scrollTitle.js
@@ -0,0 +1,20 @@
+const _confObserver = {
+    rootMargin: '-50px -50px -50px -50px',
+    threshold: [0, 0.25, 0.75, 1]
+};
+
+const addActives = document.querySelectorAll('.add-active-js');
+
+const observerAddActives = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if (entry.intersectionRatio > 0) {
+            entry.target.classList.add('active')
+        } else {
+            entry.target.classList.remove('active')
+        }
+    });
+}, _confObserver);
+
+addActives.forEach(ele => {
+    observerAddActives.observe(ele);
+});
